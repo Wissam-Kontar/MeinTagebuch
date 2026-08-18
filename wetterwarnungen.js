868fd04447c6e54f239d4cbb03ce1929b02a8809
Wissam Kontar <wissamk779@hotmail.com>
2026-08-18 21:53:03 +0200

    Implementiere Wetterwarnungen-Abonnements

---
+++ wetterwarnungen.js
@@ -0,0 +1,18 @@
+class WetterWarnungen {
+    constructor() {
+        this.abonnements = [];
+    }
+
+    abonnieren(email, regionen) {
+        this.abonnements.push({
+            email,
+            regionen
+        });
+    }
+
+    getAbonnements() {
+        return this.abonnements;
+    }
+}
+
+module.exports = WetterWarnungen;
